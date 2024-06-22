e643dc1fc3e6708923414e2e7f8dc67ad0067fb9
YeyoDev99 <yeyoduglas2004@gmail.com>
2024-06-22 10:34:00 -0500

    22-6-2024 test 1.0

--- exercise_1.py
+++ exercise_1.py
@@ -173,3 +173,49 @@ def first_and_last_position(nums,query):
         return edges
 
 evaluate_test_case(first_and_last_position, test)
+
+# You are given list of numbers, obtained by rotating a sorted list an unknown number of times. 
+# Write a function to determine the minimum number of times the original sorted list was rotated to obtain the given list. 
+# Your function should have the worst-case complexity of O(log N), where N is the length of the list. 
+# You can assume that all the numbers in the list are unique.
+# Example: The list [5, 6, 9, 0, 2, 3, 4] was obtained by rotating the sorted list [0, 2, 3, 4, 5, 6, 9] 3 times.
+# We define "rotating a list" as removing the last element of the list and adding it before the first element. E.g. rotating the list [3, 2, 4, 1] produces [1, 3, 2, 4].
+# "Sorted list" refers to a list where the elements are arranged in the increasing order e.g. [1, 3, 5, 7]
+
+
+test_1 = {
+    'input': {
+        'nums': [19, 25, 29, 3, 5, 6, 7, 9, 11, 14]
+    },
+    'output': 3
+}
+
+def evaluate(nums,mid):
+    
+    if mid - 1 >= 0 and nums[mid] < nums[mid-1]:
+        return  'found'
+    elif mid + 1 <= len(nums) - 1 and nums[mid] > nums[mid+1]:
+        return 'right'
+    elif nums[mid] >= nums[0]:
+        return 'right'
+    else:
+        return 'left'
+
+
+def binomial_search(nums):
+    high = len(nums) - 1
+    low = 0
+    while low <= high:
+        mid = (high + low)//2
+        response = evaluate(nums,mid)
+        if response == 'found':
+            return mid
+        elif response == 'right':
+            low = mid + 1
+        else:
+            high = mid - 1    
+    return 0
+
+evaluate_test_case(binomial_search, test_1)
+
+
